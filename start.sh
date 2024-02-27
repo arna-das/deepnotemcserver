ec58d79b563a0a2b5ba032e3a92f7254bdab623e
arnadas <142620137+arna-das@users.noreply.github.com>
2024-02-27 12:12:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 apt update && apt upgrade -y
 mkdir plugins
 wget https://github.com/playit-cloud/playit-minecraft-plugin/releases/latest/download/playit-minecraft-plugin.jar
+wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/4.9.2/PAPER/ViaVersion-4.9.2.jar
+wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/4.9.1/PAPER/ViaBackwards-4.9.1.jar
 mv playit-minecraft-plugin.jar plugins
-bash onthesrv.sh
+mv ViaBackwards-4.9.1.jar plugins
+mv ViaVersion-4.9.2.jar
+bash startthesrv.sh
